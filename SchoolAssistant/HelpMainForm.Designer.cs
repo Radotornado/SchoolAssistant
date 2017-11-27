595c848781f156361e8809977d673b12201357f0
Radotornado <radoslav.mandev99@gmail.com>
2017-11-28 00:49:24 +0200

    Changed design

--- SchoolAssistant/HelpMainForm.Designer.cs
+++ SchoolAssistant/HelpMainForm.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.Location = new System.Drawing.Point(13, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(633, 128);
+            this.label1.Size = new System.Drawing.Size(634, 128);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
